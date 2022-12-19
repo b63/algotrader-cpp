a0c8f4ac6fc1cb1b957a88659da96ef51aeea0c6
b63 <avink5760@gmail.com>
2022-12-18 20:40:32 -0600

    added asio and websocketpp submodules

--- main.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "algo-trader" << std::endl;
-}
---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <curl/curl.h>
+
+#define ASIO_STANDALONE
+#include <websocketpp/config/asio_no_tls_client.hpp>
+#include <websocketpp/client.hpp>
+
+
+int main(void)
+{
+  CURL *curl;
+  CURLcode res;
+
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "https://www.binance.us/api/v1/depth?symbol=ETHUSD&limit=100");
+    // verify certificate/host name
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+
+    /* Perform the request, res will get the return code */
+    res = curl_easy_perform(curl);
+    /* Check for errors */
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+
+    /* always cleanup */
+    curl_easy_cleanup(curl);
+  }
+
+  curl_global_cleanup();
+
+  return 0;
+}
