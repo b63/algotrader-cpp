a41f0e28d8e00551d319e83029db62ea2c262146
b63 <avink5760@gmail.com>
2022-12-30 22:21:44 +0000

    added python test scripts

---
+++ python/test-binance.py
@@ -0,0 +1,21 @@
+import requests
+
+headers = {"x-mbx-apikey": "bD9QfIu4FBdRJpviWI075M6KMX2lb9oUyLfC2IknlE4vcIbnFKQaeSm8f0vLW8te"}
+res = None
+
+
+def f(ticker="BNBBTC", limit=0):
+    if limit > 0:
+        url = f"https://www.binance.us/api/v1/depth?symbol={ticker}&limit={limit}"
+    else:
+        url = f"https://www.binance.us/api/v1/depth?symbol={ticker}"
+    c_res = requests.get(url, headers=headers)
+    global res
+    res = c_res
+    resj = c_res.json()
+    if "bids" in resj and "asks" in resj:
+        bids, asks = resj["bids"], resj["asks"]
+        update_id = resj["lastUpdateId"]
+        print(f"{update_id} {len(bids)} bids, {len(asks)} asks")
+    else:
+        print(resj)
---
+++ python/test-coinbase.py
@@ -0,0 +1,40 @@
+import json, hmac, hashlib, time, base64, requests
+
+SECRET_KEY = "wPIb36YUgWytnLuAMGstGGDFIRXsRCSm"
+API_KEY    = "F7lqbf8vpl39DBmf"
+URL = "https://api.coinbase.com/api/v3/brokerage/accounts/"
+
+
+def coinbase_rest_api(url, query_params=None, body=None, method="GET"):
+    timestamp = str(int(time.time())) 
+    request_path = '/' + '/'.join(url.split("/")[3:])
+
+
+    if body:
+        message = json.dumps(body)
+    else:
+        message = ""
+
+    signature_plain = timestamp + method + request_path + message
+    print(f"plain signature: {signature_plain}")
+    signature = hmac.new(SECRET_KEY.encode('utf-8'), signature_plain.encode('utf-8'), digestmod=hashlib.sha256).hexdigest()
+    headers = {
+            "CB-ACCESS-KEY": API_KEY,
+            "CB-ACCESS-TIMESTAMP": timestamp,
+            "CB-ACCESS-SIGN": signature 
+        }
+
+    response = requests.request(method, url, params=query_params, data=message, headers=headers)
+    return response
+
+
+if __name__ == "__main__":
+    res = coinbase_rest_api("https://api.coinbase.com/api/v3/brokerage/accounts")
+    print(f"headers: {res.headers}")
+    print(f"status_code: {res.status_code}")
+    print(f"raw: {res.text}")
+    try:
+        print(f"<< {json.dumps(res.json(), indent=4)}")
+    except:
+        pass
+
