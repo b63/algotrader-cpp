e000a218d5226861bd1465dcc41ebba2d34484ea
b63 <avink5760@gmail.com>
2023-01-09 21:33:31 -0600

    log stub with no new line

--- include/logger.h
+++ include/logger.h
@@ -10,6 +10,13 @@ void log(const char *fmt, Args&&... args)
     std::cout << fstr << "\n";
 }
 
+template <typename... Args>
+void log_noln(const char *fmt, Args&&... args)
+{
+    std::string fstr {std::format(std::runtime(fmt), std::forward<Args>(args)...)};
+    std::cout << fstr;
+}
+
 
 #endif
 
